dbf6e24dde474b41fa59cd6eccba9225a681337a
StuPratikNaik <pratiknaik.mca.amcec23@gmail.com>
2026-05-16 22:09:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 
 // ✅ FIX: Root route (IMPORTANT)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 
